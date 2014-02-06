ce83f88796267fa908519cb8b2a5a79bf9a95532
Sonu <sonusinh19@gmail.com>
2014-02-06 14:52:09 +0530

    New Layout

--- UI/MouseInitialization.cs
+++ UI/MouseInitialization.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Input;
 
-namespace MouseLibrary
+namespace UI
 {
-    public class Class1
+    public class MouseInitialization
     {
     }
+
 }
